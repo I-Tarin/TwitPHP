42531bd603248cffe10c0f24fa949f283e482008
I-Tarin <90906743+I-Tarin@users.noreply.github.com>
2021-11-13 00:07:05 +0500

    Add files via upload

---
+++ includes/config.php
@@ -0,0 +1,8 @@
+<?php
+
+define('SITE_NAME', 'Курлыктор');
+define('HOST', 'http://' . $_SERVER['HTTP_HOST']);
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'twitter');
+define('DB_USER', 'root');
+define('DB_PASS', '');
